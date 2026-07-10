4bae76f4460659bf07e3857f569b45df240b284e
TesheBizu <teshebizuzeleke199741118@gmail.com>
2026-07-10 22:23:48 +0300

    fix: add missing useState import in About.jsx

--- Frontend/src/pages/About/About.jsx
+++ Frontend/src/pages/About/About.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Mail, Download } from 'lucide-react';
 import { setCircularFavicon } from '../../utils/circularFavicon';
